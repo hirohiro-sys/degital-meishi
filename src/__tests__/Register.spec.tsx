17293bff818f1d6c9f2203522242edee2223f24d
hirohiro-sys <katohiro8843@icloud.com>
2024-08-18 18:47:20 +0900

    一応全部モック化

--- src/__tests__/Register.spec.tsx
+++ src/__tests__/Register.spec.tsx
@@ -13,6 +13,9 @@ jest.mock("react-router-dom", () => ({
 jest.mock("../lib/supabasefunctions", () => {
     return {
     // DBに影響を与えないようにモック化
+    getUserData: jest.fn(),
+    getSkillId: jest.fn(),
+    getSkillData: jest.fn(),
     addUser: jest.fn(),
     addUserSkill: jest.fn(),
     };
