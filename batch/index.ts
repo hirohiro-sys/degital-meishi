352c7df9309f97e06bae817787376713e2cc6a68
hirohiro-sys <katohiro8843@icloud.com>
2024-08-22 07:32:22 +0900

    模索中

--- batch/index.ts
+++ batch/index.ts
@@ -1,36 +1,35 @@
-import { supabase } from "../src/lib/supabase";
-import 'dotenv/config';
+// import { supabase } from "../src/lib/supabase";
 
 
-async function deleteUserData() {
-  const startedYesterday = new Date();
-  startedYesterday.setDate(startedYesterday.getDate() - 1);
-  startedYesterday.setUTCHours(0, 0, 0, 0);
+// async function deleteUserData() {
+//   const startedYesterday = new Date();
+//   startedYesterday.setDate(startedYesterday.getDate() - 1);
+//   startedYesterday.setUTCHours(0, 0, 0, 0);
 
-  const endedYesterday = new Date();
-  endedYesterday.setDate(endedYesterday.getDate() - 1);
-  endedYesterday.setUTCHours(23, 59, 59, 999);
+//   const endedYesterday = new Date();
+//   endedYesterday.setDate(endedYesterday.getDate() - 1);
+//   endedYesterday.setUTCHours(23, 59, 59, 999);
 
-  const userDeleteResult = await supabase
-    .from("users")
-    .delete()
-    .gte("created_at", startedYesterday.toISOString())
-    .lte("created_at", endedYesterday.toISOString());
-  console.log("startedYesterday:", startedYesterday);
-  console.log("endedYesterday:", endedYesterday);
+//   const userDeleteResult = await supabase
+//     .from("users")
+//     .delete()
+//     .gte("created_at", startedYesterday.toISOString())
+//     .lte("created_at", endedYesterday.toISOString());
+//   console.log("startedYesterday:", startedYesterday);
+//   console.log("endedYesterday:", endedYesterday);
 
-  console.log("Deleted user data:", userDeleteResult);
+//   console.log("Deleted user data:", userDeleteResult);
 
-  const skillDeleteResult = await supabase
-    .from("user_skill")
-    .delete()
-    .gte("created_at", startedYesterday.toISOString())
-    .lte("created_at", endedYesterday.toISOString());
+//   const skillDeleteResult = await supabase
+//     .from("user_skill")
+//     .delete()
+//     .gte("created_at", startedYesterday.toISOString())
+//     .lte("created_at", endedYesterday.toISOString());
 
-  console.log("Deleted skill data:", skillDeleteResult);
-}
+//   console.log("Deleted skill data:", skillDeleteResult);
+// }
 
-deleteUserData()
-  .then(() => console.log("Deletion complete!"))
-  .catch((error) => console.error("Deletion failed:", error));
+// deleteUserData()
+//   .then(() => console.log("Deletion complete!"))
+//   .catch((error) => console.error("Deletion failed:", error));
 
